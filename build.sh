4ed5b96b2b5946d14a562e4383aea0bee4eda6cc
Simon Guerout <s@sidereo.com>
2016-05-26 15:39:09 -0400

    Added the build files for the documentation.

---
+++ build.sh
@@ -0,0 +1,9 @@
+if [ -d client ]; then
+git -C client pull origin master
+else
+git clone --depth 1 --branch master https://github.com/barracksiot/ios-osx-client.git client
+fi
+
+pod update --project-directory=$PWD/client
+
+jazzy --config .jazzy.json --clean --output docs --module-version "master"
