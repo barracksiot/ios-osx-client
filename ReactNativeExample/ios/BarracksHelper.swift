dabdb6e4d9d47c4cb27006f6e89dc3c7a0d86522
pod <pod@sidereo.com>
2016-07-26 09:05:24 -0400

    Missing commits

---
+++ ReactNativeExample/ios/BarracksHelper.swift
@@ -0,0 +1,9 @@
+//
+//  BarracksHelper.swift
+//  ReactNativeExample
+//
+//  Created by CleverToday on 05/06/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ReactNativeExample/ios/BarracksHelperBridge.m
@@ -0,0 +1,9 @@
+//
+//  BarracksHelperBridge.m
+//  ReactNativeExample
+//
+//  Created by CleverToday on 05/06/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
